1d76c0d8ec4d8c9955ce56a5562c7da10059a408
zylum-admin <zylum-admin@users.noreply.github.com>
2017-01-14 14:28:22 +0000

    Create db_username.php

---
+++ dbpdo/db_username.php
@@ -0,0 +1,14 @@
+<?php
+/* It is vitally important to include the php tag above.
+   Otherwise anyone who navigates to this file on the web server will see your
+   database conection details.
+*/
+
+# Edit these values to match your own database connection details.
+# It is normal to include or require these details from a seperate file so that you can edit eisily.
+$host = "localhost";
+$dbname = "database";
+$user = "user";
+$pass = "password";
+
+# We won't put the php closing tag at the end, because this file will be included near the beginning of our code.
