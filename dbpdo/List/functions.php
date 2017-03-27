913c46e052ad7713e11146191311e3f2b77287a8
thebrentc <thebrentc@gmail.com>
2017-03-27 20:13:22 +0100

    delete...

--- dbpdo/List/functions.php
+++ dbpdo/List/functions.php
@@ -37,7 +37,7 @@ function data2htmlTable($data, $controls = true)
         if ($controls) {
             $html .= '<td>';
             $html .= '<form action = "'.$_SERVER['PHP_SELF'].'" method = "post">'."\n";
-            $html .=  '<input type = "hidden" name = "id" id = "id" value = "'.$record['ID'].'" />'."\n";
+            $html .=  '<input type = "hidden" name = "ID" id = "ID" value = "'.$record['ID'].'" />'."\n";
             $html .= '<input type = "submit" name = "submit" id = "submit" value ="Delete">'."\n";
             $html .=  '</form>'."\n"; 
             $html .= '</td>';
